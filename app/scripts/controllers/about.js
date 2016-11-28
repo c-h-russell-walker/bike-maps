9fdd5978d59f4f7ae7bc974ab3901df288ded6c9
c-h-russell-walker <c.russell.walker@gmail.com>
2016-11-28 14:10:24 -0500

    CRW Initial commit of angular app leveraging grunt - used yeoman to scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bikeMapsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bikeMapsApp
+ */
+angular.module('bikeMapsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bikeMapsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bikeMapsApp
+ */
+angular.module('bikeMapsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
